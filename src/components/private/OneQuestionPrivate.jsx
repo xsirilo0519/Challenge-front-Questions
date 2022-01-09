0dcda70da2615672b0ad2522e0c5e5999f48e47f
sebas <sebaslpez0519@gmail.com>
2022-01-09 14:45:36 -0500

    varios

--- src/components/private/OneQuestionPrivate.jsx
+++ src/components/private/OneQuestionPrivate.jsx
@@ -5,9 +5,10 @@ const OneQuestionPrivate = ({oneQuestion}) => {
     return(
         <section className='question'>
             <div className="card">
+            <div>
             <h4>{oneQuestion.question}</h4>
             <h6>{oneQuestion.category}  - <small>{oneQuestion.type}</small></h6>
-            
+            </div>
         
         </div>
         </section>
